2f8f572071e1b84d0370c313773912f59efc2bcf
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:51:26 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "app_service_slot_custom_hostname_bindings" {
+  description = "All app_service_slot_custom_hostname_binding resources"
+  value       = azurerm_app_service_slot_custom_hostname_binding.app_service_slot_custom_hostname_bindings
+}
+
