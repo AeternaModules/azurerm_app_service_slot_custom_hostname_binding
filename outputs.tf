5af793033849d134959fd1b0d21bcc366872c9fa
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:46 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "app_service_slot_custom_hostname_bindings" {
-  description = "All app_service_slot_custom_hostname_binding resources"
-  value       = azurerm_app_service_slot_custom_hostname_binding.app_service_slot_custom_hostname_bindings
-}
 output "app_service_slot_custom_hostname_bindings_app_service_slot_id" {
-  description = "List of app_service_slot_id values across all app_service_slot_custom_hostname_bindings"
-  value       = [for k, v in azurerm_app_service_slot_custom_hostname_binding.app_service_slot_custom_hostname_bindings : v.app_service_slot_id]
+  description = "Map of app_service_slot_id values across all app_service_slot_custom_hostname_bindings, keyed the same as var.app_service_slot_custom_hostname_bindings"
+  value       = { for k, v in azurerm_app_service_slot_custom_hostname_binding.app_service_slot_custom_hostname_bindings : k => v.app_service_slot_id }
 }
 output "app_service_slot_custom_hostname_bindings_hostname" {
-  description = "List of hostname values across all app_service_slot_custom_hostname_bindings"
-  value       = [for k, v in azurerm_app_service_slot_custom_hostname_binding.app_service_slot_custom_hostname_bindings : v.hostname]
+  description = "Map of hostname values across all app_service_slot_custom_hostname_bindings, keyed the same as var.app_service_slot_custom_hostname_bindings"
+  value       = { for k, v in azurerm_app_service_slot_custom_hostname_binding.app_service_slot_custom_hostname_bindings : k => v.hostname }
 }
 output "app_service_slot_custom_hostname_bindings_ssl_state" {
-  description = "List of ssl_state values across all app_service_slot_custom_hostname_bindings"
-  value       = [for k, v in azurerm_app_service_slot_custom_hostname_binding.app_service_slot_custom_hostname_bindings : v.ssl_state]
+  description = "Map of ssl_state values across all app_service_slot_custom_hostname_bindings, keyed the same as var.app_service_slot_custom_hostname_bindings"
+  value       = { for k, v in azurerm_app_service_slot_custom_hostname_binding.app_service_slot_custom_hostname_bindings : k => v.ssl_state }
 }
 output "app_service_slot_custom_hostname_bindings_thumbprint" {
-  description = "List of thumbprint values across all app_service_slot_custom_hostname_bindings"
-  value       = [for k, v in azurerm_app_service_slot_custom_hostname_binding.app_service_slot_custom_hostname_bindings : v.thumbprint]
+  description = "Map of thumbprint values across all app_service_slot_custom_hostname_bindings, keyed the same as var.app_service_slot_custom_hostname_bindings"
+  value       = { for k, v in azurerm_app_service_slot_custom_hostname_binding.app_service_slot_custom_hostname_bindings : k => v.thumbprint }
 }
 output "app_service_slot_custom_hostname_bindings_virtual_ip" {
-  description = "List of virtual_ip values across all app_service_slot_custom_hostname_bindings"
-  value       = [for k, v in azurerm_app_service_slot_custom_hostname_binding.app_service_slot_custom_hostname_bindings : v.virtual_ip]
+  description = "Map of virtual_ip values across all app_service_slot_custom_hostname_bindings, keyed the same as var.app_service_slot_custom_hostname_bindings"
+  value       = { for k, v in azurerm_app_service_slot_custom_hostname_binding.app_service_slot_custom_hostname_bindings : k => v.virtual_ip }
 }
 
